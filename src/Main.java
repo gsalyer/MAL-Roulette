92125d22d1a49791c8d4ba7e6b063eaaf7e68e3d
Garrett Salyer <garrettasalyer@gmail.com>
2020-05-05 15:03:52 -0700

    redoing this again

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         MyList m = new MyList("syblix");
         System.out.println(m.getTitle(m.getRand()));
