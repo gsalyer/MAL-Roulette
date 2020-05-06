90c7c0857d644f3fccf989793b6b007f2592e5f3
Garrett Salyer <garrettasalyer@gmail.com>
2020-05-05 17:25:53 -0700

    now accepts user input from the console

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        MyList m = new MyList("syblix");
-        System.out.println(m.getTitle(m.getRand()));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter MAL username");
+        MyList m = new MyList(scan.next());
+
+        do
+        {
+            System.out.println(m.getTitle(m.getRand()));
+            System.out.println("Enter r for another one");
+        }
+        while(scan.next().equalsIgnoreCase("r"));
     }
 }
